817bab01c4c303c1b446b9d5a3a7bcd9557a93d1
Roromo0306 <pownyrainbow03@gmail.com>
2026-01-30 19:29:35 +0100

    Main Menu + 2D animation package

---
+++ SevillaJam5_GGJ/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject pressAnyKey;
+    public GameObject mainMenu;
+    public GameObject creditsPanel;
+    public GameObject tutorialPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(false);
+        pressAnyKey.SetActive(true);
+        creditsPanel.SetActive(false);
+        tutorialPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            pressAnyKey.SetActive(false);
+            mainMenu.SetActive(true);
+        }   
+    }
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void CreditosAbrir()
+    {
+        mainMenu.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void TutorialAbrir()
+    {
+        mainMenu.SetActive(false);
+        tutorialPanel.SetActive(true);
+    }
+
+    public void Cerrar()
+    {
+        mainMenu.SetActive(true);
+        creditsPanel.SetActive(false);
+        tutorialPanel.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
