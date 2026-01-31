a92ad669c9ab7f6305d2347c4ecab0ffc155267d
Roromo0306 <pownyrainbow03@gmail.com>
2026-01-31 18:53:58 +0100

    Paneles de Información

---
+++ SevillaJam5_GGJ/Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableImage : MonoBehaviour
+{
+    [Header("Imagen que mostrará este punto")]
+    public Sprite imageToShow;
+
+    [Header("UI")]
+    public GameObject panelUI;
+    public Image panelImage;
+
+    private bool playerInside;
+
+    private void Awake()
+    {
+        panelUI.SetActive(false);    
+    }
+
+    void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.F))
+        {
+            TogglePanel();
+        }
+    }
+
+    void TogglePanel()
+    {
+        bool isActive = panelUI.activeSelf;
+        panelUI.SetActive(!isActive);
+
+        if (!isActive)
+        {
+            panelImage.sprite = imageToShow;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+            panelUI.SetActive(false);
+        }
+    }
+
+    public void ClosePanel()
+    {
+        panelUI.SetActive(false);
+    }
+}
